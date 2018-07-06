1c6582034ac8dbd36c267f8a8364c8a5c065eafb
Eli <fatcalf9701@gmail.com>
2018-07-06 09:29:59 -0600

    game is done

--- python/main.py
+++ python/main.py
@@ -1,15 +1,15 @@
 import random
 import sys
 name = input("who dares challenge the MITs? ")
-health = 60
-
+health = 80
+maxHealth = 120
 
 class MIT:
     """MITs - The Enemies."""
     def __init__(self, name):
         self.name = name
         self.health = random.randint(1,15)
-        self.damage = random.randint(5,15)
+        self.damage = random.randint(5,14)
 
     def isAlive(self):
         return self.health > 0
@@ -20,8 +20,10 @@ class MIT:
         return damage
 
 class Mentor(MIT):
-    def __init__(self, name):
-        super().__init__(self, name)
+    def __init__(self, name, health, damage):
+        super().__init__(name)
+        self.health = health
+        self.damage = damage
 
 mits = [
     MIT("kaitlen"),
@@ -29,7 +31,7 @@ mits = [
     MIT("austin"),
     MIT("rhi rhi"),
     MIT("travis")
-        ]
+]
 
 
 score = 0
@@ -50,13 +52,18 @@ while len(mits) > 0:
             if health < 0:
                 print("game over")
                 sys.exit(0)
-        elif action.lower() == "rest":
-            rest = random.randint(5,11)
+        elif action.lower() == "rest" and health <= maxHealth:
+            rest = random.randint(6,18)
             health += rest
+            if health > maxHealth:
+                 health = maxHealth
             print("you have rested and healed {} health".format(rest))
             health -= mit.damage
             print("{} did {} damage".format(mit.name, mit.damage))
             print("{} has {} health ".format(mit.name, mit.health))
+        elif action.lower() == "rest" and health == maxHealth:
+            print("You cant heal anymore!")
+
         else:
             Caught = random.randint(1,5) == 1
             if not Caught:
@@ -66,14 +73,69 @@ while len(mits) > 0:
             else:
                 print("you failed to run away!")
 
+maxHealth = maxHealth + 150
+health = health + 150
+print("the spirits of the slain mits have blessed you with plus 150 health")
+mentors = [
+    Mentor("cbreeze", 15, 17),
+    Mentor("brett", 40, 20)
+]
 
-health = health + 50
+
 
 print("you have {} health".format(health))
 print("prepare to enter the dungeon of the mits")
 print("oh no before you fight the final boss (brett) you must fight his minion the mighty c breeze")
 print("the Mentor c breeze appears")
 
-print("you have {} health".format(health))
-print("do you want to fight or rest")
-action = input("fight/rest >")
+
+mit = mentors[0]
+
+while mit.isAlive():
+    print("you have {} health".format(health))
+    print("do you want to fight or rest")
+    action = input("fight/rest >")
+    if action.lower() != "fight" "retreat" "rest":
+        print("That is not an option!")
+    if action.lower() == "fight":
+                damage = mit.attack()
+                score += damage
+                print("you did {} damage!".format(damage))
+                print("{} has {} health ".format(mit.name, mit.health))
+                health -= mit.damage
+                if health < 0:
+                    print("game over")
+                    sys.exit(0)
+    elif action.lower() == "rest":
+                rest = random.randint(5,11)
+                health += rest
+                print("you have rested and healed {} health".format(rest))
+                health -= mit.damage
+
+
+if mentors[0].health <= 0:
+
+                mit = mentors[1]
+                print("prepare to face the mighty brett")
+
+                while mit.isAlive():
+                    print("you have {} health".format(health))
+                    print("do you want to fight or rest")
+                    action = input("fight/rest >")
+                    if action.lower() == "fight":
+                                damage = mit.attack()
+                                score += damage
+                                print("you did {} damage!".format(damage))
+                                print("{} has {} health ".format(mit.name, mit.health))
+                                health -= mit.damage
+                                if health < 0:
+                                    print("game over")
+                                    sys.exit(0)
+                    elif action.lower() == "rest":
+                                rest = random.randint(5,11)
+                                health += rest
+                                print("you have rested and healed {} health".format(rest))
+                                health -= mit.damage
+
+if mentors[1].health <= 0:
+                                print("you win")
